872fca8dd264ab280cef30577ceb49fac305638f
Joe Passafiume <joepass456@gmail.com>
2020-09-29 18:48:05 -0400

    version 0.8

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-//document.body.innerHTML = "<p>Hello World</p>";
-//document.getElementById("main").innerHTML = 5 + 6;
-//console.log("test");
-//document.getElementById("main").innerHTML = "hello world";
+// declare variables - link html
+const city = document.querySelector('.city');
+const kel = document.querySelector('.kel');
+const deg = document.querySelector('.deg');
+const cel = document.querySelector('.cel');
+const con = document.querySelector('.con');
+const pic = document.querySelector('.pic');
 
-//document.write(5 + 6);
-//api.openweathermap.org/data/2.5/weather?zip={40356},&appid={d9608784e9e57ff730dbec18ce772f3a};
 var webAddress = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 var webID = ',us&appid=d9608784e9e57ff730dbec18ce772f3a';
 var data;
-
+document.getElementById('turnon').style.visibility='hidden'
 
 async function getWeather(zip) {
     let response = await fetch(webAddress + zip + webID);
@@ -27,16 +28,24 @@ async function getZipcode() {
         alert('Get weather info for zipcode ' + zipcode + '?');        
         data = await getWeather(zipcode);
     }
+    // push elements to html
+    document.getElementById('turnon').style.visibility='visible'
+    city.innerHTML = data.name;
+    kel.innerHTML = Math.floor(data.main.temp);
+    deg.innerHTML = Math.floor((data.main.temp - 273.15) * 9 / 5 + 32);
+    cel.innerHTML = Math.floor(data.main.temp - 273.15) + " " + "C";
+    con.innerHTML = data.weather[0].description;
+    var img = document.createElement("img");
+    let str = data.weather[0].icon;
+    console.log(str);
+    img.src = "http://openweathermap.org/img/wn/" + str + "@2x.png";
+    //img.width = width;
+    //img.height = height;
+    //img.alt = alt;
+
+    // This next line will just add it to the <body> tag
+    pic.appendChild(img);
+
 }
 
 
-// getWeather().then(data => console.log(data));
-
-// async function getUsers() {
-    // let response = await fetch(
-    //   "api.openweathermap.org/data/2.5/weather?zip=40356&appid=d9608784e9e57ff730dbec18ce772f3a"
-    // );
-    // let data = await response.json();
-    // return data;
-//}
-// getUsers().then(data => console.log(data));
